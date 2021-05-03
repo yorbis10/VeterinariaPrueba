5612d11f5151368d209d9ec0911c1b533ddd9eb1
yorbarbe <yorbarbe@3RO-PLECO5CD847.suramericana.com.co>
2021-05-03 10:45:43 -0500

    pregamacion de archivo conexion

---
+++ src/Configuracion/Conexion.java
@@ -0,0 +1,29 @@
+package Configuracion;
+
+import java.sql.*;
+
+public class Conexion {
+
+    public static Connection getConexion() {
+        Connection con = null;
+
+        try {
+
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            con = DriverManager.getConnection("jdbc:sqlserver://localhost:1433;"
+                    + "database=Veterinaria;user=admin;password=admin");
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        }
+
+        return con;
+
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(Conexion.getConexion());
+        
+    }
+}
