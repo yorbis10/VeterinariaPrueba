3bd9314de370944ddd87b0eb78828731e14d0749
yorbarbe <yorbarbe@3RO-PLECO5CD847.suramericana.com.co>
2021-05-03 10:53:38 -0500

    pregamacion de archivo conexion

---
+++ src/Entidades/Usuarios.java
@@ -0,0 +1,66 @@
+
+package Entidades;
+
+
+public class Usuarios {
+     private int codigo;
+    private String nombre, apellido1,apellido2,usuario,clave;
+    
+    public Usuarios (){ }
+    
+    public Usuarios (String usuario, String clave){
+           
+    
+    this.usuario=usuario;
+    this.clave=clave;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido1() {
+        return apellido1;
+    }
+
+    public void setApellido1(String apellido1) {
+        this.apellido1 = apellido1;
+    }
+
+    public String getApellido2() {
+        return apellido2;
+    }
+
+    public void setApellido2(String apellido2) {
+        this.apellido2 = apellido2;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+    
+}
