cf1da819b3e3b7047e290506490001d1ec91f76a
yorbarbe <yorbarbe@3RO-PLECO5CD847.suramericana.com.co>
2021-05-14 13:34:27 -0500

    creacion de clase datos

---
+++ src/Controlador/datos.java
@@ -0,0 +1,15 @@
+
+package Controlador;
+
+public class datos {
+    
+    public boolean validarusuario(String usuario, String clave){
+if(usuario.equals("prueba") && clave.equals("123")){
+return true;
+}
+else{
+return false;
+}
+}
+    
+}
