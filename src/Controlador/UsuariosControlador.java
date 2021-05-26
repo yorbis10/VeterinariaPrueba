54c105faef515ba0b276f4953fd5bd2e8849d76e
yorbarbe <yorbarbe@3RO-PLECO5CD847.suramericana.com.co>
2021-05-25 23:23:10 -0500

    modificacion de erro al ingresar usurio y clave incorrecto, ingresa con el boton enter

--- src/Controlador/UsuariosControlador.java
+++ src/Controlador/UsuariosControlador.java
@@ -6,9 +6,12 @@ import Vista.frmLogin;
 import Vista.frmPrincipal;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.JOptionPane;
 import javax.swing.Timer;
 
-public class UsuariosControlador implements ActionListener {
+public class UsuariosControlador implements ActionListener, KeyListener {
 
     UsuarioDAO lDao = new UsuarioDAO();
     Usuarios usu;
@@ -19,30 +22,43 @@ public class UsuariosControlador implements ActionListener {
     public UsuariosControlador(frmLogin login) {
         this.fLogin = login;
         this.fLogin.btnIngresar.addActionListener(this);
+        this.fLogin.txtClave.addKeyListener(this);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
         if (e.getSource().equals(fLogin.btnIngresar)) {
-            String usuario = fLogin.txtUsuario.getText();
-            String clave = new String(fLogin.txtClave.getPassword());
-            usu = new Usuarios(usuario, clave);
-            usuarioGlobal = lDao.login(usu);
-            if (!usuarioGlobal.equals("")) {
-                frmPrincipal fPrincipal = new frmPrincipal();
-                fPrincipal.setVisible(true);
-                fLogin.dispose();
-            } else {
-                //JOptionPane.showMessageDialog(null, "Usuario y clave Incorrecto");
-                fLogin.lblError.setText("Usuario y clave Incorrecto");
-                fLogin.txtUsuario.setText("");
-                fLogin.txtClave.setText("");
-                fLogin.txtUsuario.requestFocus();
-                ocultarlblerror();
-            }
+            ingresar();
+
         }
 
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+            ingresar();
+        }
+    }
+
+    // metodo para ingresar
+    public void ingresar() {
+        String usuario = fLogin.txtUsuario.getText();
+        String clave = new String(fLogin.txtClave.getPassword());
+        usu = new Usuarios(usuario, clave);
+        usuarioGlobal = lDao.login(usu);
+
+        if (!usuarioGlobal.equals("")) {
+            frmPrincipal fPrincipal = new frmPrincipal();
+            fPrincipal.setVisible(true);
+            fLogin.dispose();
+        } else {
+            fLogin.lblError.setText("¡Usuario o clave incorrectos!");
+            fLogin.txtUsuario.setText("");
+            fLogin.txtClave.setText("");
+            fLogin.txtUsuario.requestFocus();
+            ocultarlblerror();
+        }
     }
 
     // metodo para ocutar lbl del error login
@@ -56,5 +72,13 @@ public class UsuariosControlador implements ActionListener {
         };
         new Timer(delay, taskPerformer).start();
 
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
     }
 }
