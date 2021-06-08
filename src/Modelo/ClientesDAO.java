7e4dfd4c8a672cb3492d18f90853cb44c9f2adc6
yorbis10 <yorbis10@3RO-PLECO5CD847.suramericana.com.co>
2021-06-07 20:45:04 -0500

    creacion de listar clientes

--- src/Modelo/ClientesDAO.java
+++ src/Modelo/ClientesDAO.java
@@ -1,7 +1,78 @@
 
 package Modelo;
 
+import Configuracion.Conexion;
+import Entidades.Cliente;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class ClientesDAO {
+    public static final String LISTAR = "select Documento,Tipo_Documento,Nombre,Apellidos,Telefono,Direccion,Ciudad,Corrreo,Observacion  from Cliente";
+    
+    
+    //guarda la conexion a la bd
+    Connection con = null;
+
+    //preparar la consulta a la BD
+    PreparedStatement ps = null;
+
+    //Guarda el resultado de la consulta
+    ResultSet rs = null;
+    
+    Cliente clien;
+    
+    // metodo para listar los registros en la tabla clientes
+    public List<Cliente> Listar() {
+        
+        List<Cliente> lCliente = new ArrayList<>();
+        
+        try {
+            //conectar a la BD
+            con = Conexion.getConexion();
+
+            //preparamos la consulta
+            ps = con.prepareStatement(LISTAR);
+
+            // ejecutamos la consulta
+            rs = ps.executeQuery();
+            
+            while (rs.next()) {
+                
+                int Documento = rs.getInt("Documento");
+                String Tipo_Documento = rs.getString("Tipo_Documento");
+                String Nombre = rs.getString("Nombre");
+                String Aprellidos = rs.getString("Apellidos");
+                int Telefono = rs.getInt("Telefono");
+                String Direccion = rs.getString("Direccion");
+                String Ciudad = rs.getString("Ciudad");
+                String Correo = rs.getString("Corrreo");
+                String Observacion = rs.getString("Observacion");
+                
+                clien = new Cliente(Documento, Tipo_Documento, Nombre, Aprellidos, Telefono, Direccion, Ciudad, Correo, Observacion);
+
+                // agregarlo a la lista
+                lCliente.add(clien);
+                 System.out.println("lista organizada");
+                System.out.println("listado de clientes" + lCliente);
+            }
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            Conexion.cerrar(con);
+        }
+        return lCliente;
+        
+    }
+    
+    
+    
+    
+    
+    
     
 }
