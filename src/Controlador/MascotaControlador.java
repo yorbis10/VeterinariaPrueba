0fa3e655eab54126e654d18e56a551328d900255
yorbis10 <yorbis10@3RO-PLECO5CD847.suramericana.com.co>
2021-06-09 10:50:52 -0500

    metodo listar

--- src/Controlador/MascotaControlador.java
+++ src/Controlador/MascotaControlador.java
@@ -3,6 +3,7 @@ package Controlador;
 
 import Entidades.Mascota;
 import Modelo.MascotasDAO;
+
 import Vista.frmMascotas;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -12,7 +13,7 @@ import javax.swing.table.DefaultTableModel;
 
 public class MascotaControlador implements ActionListener {
 
-    MascotasDAO mascotadao = new MascotasDAO();
+    MascotasDAO MascotasDAO = new MascotasDAO();
         frmMascotas frmasc = new frmMascotas();
         Mascota mas = new Mascota();
         DefaultTableModel modelo = new DefaultTableModel();
@@ -26,7 +27,7 @@ public class MascotaControlador implements ActionListener {
             modelo = (DefaultTableModel) tabla.getModel();
             
             
-            
+            List<Mascota> lmascota = MascotasDAO.Listar();
             
            
             
